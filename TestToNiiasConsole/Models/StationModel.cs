d04b4ab9e6d72600e52640bdba96c3734556f390
Single_Astronaut <nshendelov@gmail.com>
2024-01-22 17:44:19 +0400

    add: Модель, репозиторий. TODO: хардкод станций в Program, заливка, вывод списка вершин и сегментов станции.

---
+++ TestToNiiasConsole/Models/StationModel.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace TestToNiiasConsole.Models
+{
+    /// <summary>
+    /// Точки.
+    /// </summary>
+    public class Point
+    {
+        /// <summary>
+        /// Идентификатор точки.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название точки.
+        /// </summary>
+        public string Name { get; set; }
+
+        public Point(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    /// <summary>
+    /// Участок.
+    /// </summary>
+    public class Segment
+    {
+        /// <summary>
+        /// Идентификатор участка.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название участка.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Начальная точка участка.
+        /// </summary>
+        public Point StartPoint { get; set; }
+
+        /// <summary>
+        /// Конечная точка участка.
+        /// </summary>
+        public Point EndPoint { get; set; }
+
+        /// <summary>
+        /// Расстояние между начальной и конечной точкой участка.
+        /// </summary>
+        public double Length { get; set; }
+
+        public Segment(int id, string name, Point startPoint, Point endPoint, double length)
+        {
+            Id = id;
+            Name = name;
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            Length = length;
+        }
+    }
+
+    /// <summary>
+    /// Путь, состоит из участков.
+    /// </summary>
+    public class Path
+    {
+        /// <summary>
+        /// Идентификатор пути.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название пути.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Список участков, из которых состоит путь.
+        /// </summary>
+        public List<Segment> Segments { get; set; }
+
+        public Path(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Segments = new List<Segment>();
+        }
+    }
+
+    /// <summary>
+    /// Парк, может включать в себя несколько путей.
+    /// </summary>
+    public class Park
+    {
+        /// <summary>
+        /// Идентификатор парка.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Название парка.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Список путей, входящих в парк.
+        /// </summary>
+        public List<Path> Paths { get; set; }
+
+        public Park(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Paths = new List<Path>();
+        }
+    }
+
+    /// <summary>
+    /// Модель станции.
+    /// Станция может включать в себя парки, отдельные пути и участки.
+    /// </summary>
+    public class StationModel
+    {
+        /// <summary>
+        /// Список участков вне путей.
+        /// </summary>
+        public List<Segment> Segments { get; set; }
+
+        /// <summary>
+        /// Список путей вне парка.
+        /// </summary>
+        public List<Path> Paths { get; set; }
+
+        /// <summary>
+        /// Список парков.
+        /// </summary>
+        public List<Park> Parks { get; set; }
+
+        /// <summary>
+        /// Список смежности для хранения структуры графа станции.
+        /// </summary>
+        public Dictionary<Point, List<Segment>> AdjacencyList { get; set; }
+
+        public StationModel()
+        {
+            Segments = new List<Segment>();
+            Paths = new List<Path>();
+            Parks = new List<Park>();
+            AdjacencyList = new Dictionary<Point, List<Segment>>();
+        }
+
+        /// <summary>
+        /// Обновление списка смежности.
+        /// </summary>
+        public static void UpdateAdjacencyList(StationModel station)
+        {
+            // Построение списков смежности на основе сегментов станции
+            station.AdjacencyList = BuildAdjacencyList(station.Segments);
+        }
+
+        // Метод для построения списков смежности
+        private static Dictionary<Point, List<Segment>> BuildAdjacencyList(List<Segment> segments)
+        {
+            // Словарь, который будет содержать списки смежности для каждой точки
+            Dictionary<Point, List<Segment>> adjacencyList = new Dictionary<Point, List<Segment>>();
+
+            // Проход по всем сегментам станции для построения списков смежности
+            foreach (var segment in segments)
+            {
+                // Если точка старта сегмента не существует в словаре, добавляем ее
+                if (!adjacencyList.ContainsKey(segment.StartPoint))
+                {
+                    adjacencyList[segment.StartPoint] = new List<Segment>();
+                }
+
+                // Если точка конца сегмента не существует в словаре, добавляем ее
+                if (!adjacencyList.ContainsKey(segment.EndPoint))
+                {
+                    adjacencyList[segment.EndPoint] = new List<Segment>();
+                }
+
+                // Добавляем сегменты в списки смежности соответствующих точек
+                adjacencyList[segment.StartPoint].Add(segment);
+                adjacencyList[segment.EndPoint].Add(segment);
+            }
+
+            return adjacencyList;
+        }
+    }
+}
---
+++ TestToNiiasConsole/Repositories/StationReposirory.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestToNiiasConsole.Models;
+using static System.Collections.Specialized.BitVector32;
+
+namespace TestToNiiasConsole.Repositories
+{
+    public class StationReposirory
+    {
+        /// <summary>
+        /// Метод для поиска кратчайшего пути между двумя точками на станции
+        /// </summary>
+        /// <param name="station"> Станция. </param>
+        /// <param name="start"> Начальная точка.</param>
+        /// <param name="end"> Конечная точка. </param>
+        /// <returns> Возвращает массив точек кратчайшего маршрута и общую длину. </returns>
+        public static Tuple<Point[], double> FindShortestPath(StationModel station, Point start, Point end)
+        {
+            // Обновляем списки смежности
+            StationModel.UpdateAdjacencyList(station);
+
+            Dictionary<Point, double> distances = new Dictionary<Point, double>();
+            Dictionary<Point, Point> previous = new Dictionary<Point, Point>();
+            HashSet<Point> vertices = new HashSet<Point>(station.AdjacencyList.Keys);
+
+            // Инициализация расстояний и предыдущих вершин
+            foreach (var point in station.AdjacencyList.Keys)
+            {
+                distances[point] = double.PositiveInfinity;
+                previous[point] = null;
+            }
+
+            // Расстояние от начальной точки до самой себя равно 0
+            distances[start] = 0;
+
+            while (vertices.Count > 0)
+            {
+                var current = vertices.OrderBy(v => distances[v]).First();
+                vertices.Remove(current);
+
+                if (distances[current] == double.PositiveInfinity)
+                {
+                    break; // Если расстояние до текущей вершины равно бесконечности, значит, не существует пути
+                }
+
+                foreach (var neighborSegment in station.AdjacencyList[current])
+                {
+                    var neighborPoint = neighborSegment.EndPoint;
+                    var alternativeDistance = distances[current] + neighborSegment.Length;
+
+                    if (alternativeDistance < distances[neighborPoint])
+                    {
+                        distances[neighborPoint] = alternativeDistance;
+                        previous[neighborPoint] = current;
+                    }
+                }
+            }
+
+            // Построение массива точек пути от конечной точки к начальной
+            var path = new List<Point>();
+            var currentPoint = end;
+
+            while (currentPoint != null)
+            {
+                path.Insert(0, currentPoint);
+
+                if (currentPoint != start)
+                {
+                    var edge = station.AdjacencyList[currentPoint].FirstOrDefault(s => Math.Abs(distances[currentPoint] - (distances[s.EndPoint] + s.Length)) < double.Epsilon);
+
+                    if (edge != null)
+                    {
+                        currentPoint = edge.StartPoint;
+                    }
+                    else
+                    {
+                        // Не удалось восстановить путь
+                        Console.WriteLine("Путь не существует.");
+                        return null;
+                    }
+                }
+                else
+                {
+                    currentPoint = null;
+                }
+            }
+
+            // Вычисление длины маршрута
+            double totalDistance = distances[end];
+
+            return new Tuple<Point[], double>(path.ToArray(), totalDistance);
+        }
+
+        /// <summary>
+        /// Вывод всех вершин, принадлежащих конкретному парку.
+        /// </summary>
+        /// <param name="station"></param>
+        /// <param name="parkName"></param>
+        public static void PrintParkVertices(StationModel station, int parkId)
+        {
+            // Проверяем, что станция не является null
+            if (station == null)
+            {
+                Console.WriteLine("Станция не инициализирована.");
+                return;
+            }
+
+            // Создаем множество для хранения вершин парка
+            HashSet<Point> parkVertices = new HashSet<Point>();
+
+            string parkName = "";
+            // Проходим по всем паркам
+            foreach (var park in station.Parks)
+            {
+                // Проверяем, что текущий парк имеет нужное имя
+                if (park.Id == parkId)
+                {
+                    // Проходим по всем путям в парке
+                    foreach (var path in park.Paths)
+                    {
+                        // Проходим по всем участкам в пути и добавляем вершины в множество
+                        foreach (var segment in path.Segments)
+                        {
+                            parkVertices.Add(segment.StartPoint);
+                            parkVertices.Add(segment.EndPoint);
+                        }
+                    }
+                }
+                parkName = park.Name;
+            }
+
+            // Выводим вершины парка
+            Console.WriteLine($"Вершины парка '{parkName}':");
+            foreach (var vertex in parkVertices)
+            {
+                Console.WriteLine($"{vertex.Id}, {vertex.Name}");
+            }
+        }
+    }
+}
